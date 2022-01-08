b1cf46795b19e329e3d4252fcfd105a4adabf200
zoltan_lovasz <zoltan_lovasz@epam.com>
2022-01-08 19:09:01 +0100

    static web server

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//adding the Express middleware to serve static files
+app.use(express.static(path.join(__dirname, './static')));
 
 const port = 3000;
 // thats a collback (request,response)
 app.get('/',(request, response) => {
-    response.send('Hello world');
+    //response.send('Hello world');
+    //instead of sending text, send page
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers',(request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 
