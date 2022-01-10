c5ec1853cb0458744739c27ff4659edca82c0599
zoltan_lovasz <zoltan_lovasz@epam.com>
2022-01-10 18:58:20 +0100

    adding nodemon for live reload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 //eslint: npx eslint --init
 
-
 const app = express();
 
 //adding the Express middleware to serve static files
@@ -11,18 +10,17 @@ app.use(express.static(path.join(__dirname, './static')));
 
 const port = 3000;
 // thats a collback (request,response)
-app.get('/',(request, response) => {
-    //response.send('Hello world');
-    //instead of sending text, send page
-    response.sendFile(path.join(__dirname, './static/index.html'));
+app.get('/', (request, response) => {
+  //response.send('Hello world');
+  //instead of sending text, send page
+  response.sendFile(path.join(__dirname, './static/index.html'));
 });
 
-app.get('/speakers',(request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'));
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
-
-app.listen(port, ()=>{
-    //backticks because of string interpolation
-    console.log(`Server started on ${port}`);
-} );
+app.listen(port, () => {
+  //backticks because of string interpolation
+  console.log(`Server started on ${port}`);
+});
